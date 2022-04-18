34cb5781aeba49d4ad00bb5283886501a8fb64a4
shayonkeating <51486690+shayonkeating@users.noreply.github.com>
2022-04-18 09:14:52 -0700

    updated arrays.py

--- arrays.py
+++ arrays.py
@@ -1,10 +1,34 @@
-# Testing arrays
+# Testing arrays and vectors
 
 def array_test():
     ar = [1, 3, 5, 7, 9]
 
-    # removes and returns last object in list
+    # removes and return last object in list
     ar.pop()
 
     # appeneds element to end of list
-    ar.append()
+    ar.append(11)
+
+    print(ar)
+    print("Index of 5:", ar.index(3)) # index place of given value
+
+    # remove first occurence of item with given valuen
+    ar.remove(5)
+    print("Removed 5:", ar)
+
+    # reverse the array
+    ar.reverse()
+    print("Reversed:", ar)
+    # sort the array after reversal then print
+    print("Sorted Return:", sorted(ar))
+
+    ar.sort()
+    print("Sorted original: ", ar)
+
+# add functions to main
+def main():
+    array_test()
+
+if __name__ == "__main__":
+    main()
+    print("Task Finished")
