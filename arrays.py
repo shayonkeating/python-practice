181a1ddeaaee9321ca1a88d1cd011cdaa1784504
shayonkeating <51486690+shayonkeating@users.noreply.github.com>
2022-04-18 08:43:40 -0700

    added arrays

--- arrays.py
+++ arrays.py
@@ -1,2 +1,10 @@
-#arrays
-array = array
+# Testing arrays
+
+def array_test():
+    ar = [1, 3, 5, 7, 9]
+
+    # removes and returns last object in list
+    ar.pop()
+
+    # appeneds element to end of list
+    ar.append()
